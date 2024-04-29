8e51076a8d8d749b0484f59f742e1a3f791b8f55
pgm-jerodeno <jeroen.denoyette@student.arteveldehs.be>
2024-04-29 09:39:14 +0200

    added win message

--- scripts/app.js
+++ scripts/app.js
@@ -72,43 +72,46 @@ function generateGridValuesHTML(gridValues) {
 
 function checkWin(gridValues) {
   if(gridValues[0].value == 'X' && gridValues[1].value == 'X' && gridValues[2].value == 'X') {
-    showWin("Player1 wins!");
+    showWin("Player 1 wins!");
   } else if(gridValues[3].value == 'X' && gridValues[4].value == 'X' && gridValues[5].value == 'X') {
-    alert("Player1 wins!");
+    showWin("Player 1 wins!");
   } else if(gridValues[6].value == 'X' && gridValues[7].value == 'X' && gridValues[8].value == 'X') {
-    alert("Player1 wins!");
+    showWin("Player 1 wins!");
   } else if(gridValues[0].value == 'X' && gridValues[3].value == 'X' && gridValues[6].value == 'X') {
-    alert("Player1 wins!");
+    showWin("Player 1 wins!");
   } else if(gridValues[1].value == 'X' && gridValues[4].value == 'X' && gridValues[7].value == 'X') {
-    alert("Player1 wins!");
+    showWin("Player 1 wins!");
   } else if(gridValues[2].value == 'X' && gridValues[5].value == 'X' && gridValues[8].value == 'X') {
-    alert("Player1 wins!");
+    showWin("Player 1 wins!");
   } else if(gridValues[0].value == 'X' && gridValues[4].value == 'X' && gridValues[8].value == 'X') {
-    alert("Player1 wins!");
+    showWin("Player 1 wins!");
   } else if(gridValues[2].value == 'X' && gridValues[4].value == 'X' && gridValues[6].value == 'X') {
-    alert("Player1 wins!");
+    showWin("Player 1 wins!");
   } else if(gridValues[0].value == 'O' && gridValues[1].value == 'O' && gridValues[2].value == 'O') {
-    alert("Player2 wins!");
+    showWin("Player 2 wins!");
   } else if(gridValues[3].value == 'O' && gridValues[4].value == 'O' && gridValues[5].value == 'O') {
-    alert("Player2 wins!");
+    showWin("Player 2 wins!");
   } else if(gridValues[6].value == 'O' && gridValues[7].value == 'O' && gridValues[8].value == 'O') {
-    alert("Player2 wins!");
+    showWin("Player 2 wins!");
   } else if(gridValues[0].value == 'O' && gridValues[3].value == 'O' && gridValues[6].value == 'O') {
-    alert("Player2 wins!");
+    showWin("Player 2 wins!");
   } else if(gridValues[1].value == 'O' && gridValues[4].value == 'O' && gridValues[7].value == 'O') {
-    alert("Player2 wins!");
+    showWin("Player 2 wins!");
   } else if(gridValues[2].value == 'O' && gridValues[5].value == 'O' && gridValues[8].value == 'O') {
-    alert("Player2 wins!");
+    showWin("Player 2 wins!");
   } else if(gridValues[0].value == 'O' && gridValues[4].value == 'O' && gridValues[8].value == 'O') {
-    alert("Player2 wins!");
+    showWin("Player 2 wins!");
   } else if(gridValues[2].value == 'O' && gridValues[4].value == 'O' && gridValues[6].value == 'O') {
-    alert("Player2 wins!");
+    showWin("Player 2 wins!");
+  } else if(gridValues[0].value !== null && gridValues[1].value !== null && gridValues[2].value !== null && gridValues[3].value !== null && gridValues[4].value !== null && gridValues[5].value !== null && gridValues[6].value !== null && gridValues[7].value !== null && gridValues[8].value !== null) {
+    showWin("It's a tie!");
   }
 }
 
 function showWin(message) {
   $win = document.querySelector(".win");
   $win.querySelector('.message').innerHTML = message;
+  $win.classList.remove('hidden');
 }
 
 updateGridValues(gridValues);
